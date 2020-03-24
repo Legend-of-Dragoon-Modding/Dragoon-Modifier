b551bf2cfe46e09fa6d9ed5739e4f81d035ee9a2
Zychronix <zychronix1@gmail.com>
2020-03-23 20:06:20 -0400

    Duo Character Mode

--- Dragoon Modifier/Scripts/[ALL] Battle Controller.cs
+++ Dragoon Modifier/Scripts/[ALL] Battle Controller.cs
@@ -13,7 +13,7 @@ public class BattleController {
     public static void Run(Emulator emulator) {
         int encounterValue = emulator.ReadShort(Constants.GetAddress("BATTLE_VALUE"));
         if (emulator.ReadByteU(0x9A7E6 + Constants.OFFSET) == 152) {
-            if (Globals.PARTY_SLOT[0] != 0) {
+            if (Globals.PARTY_SLOT[0] != 0 && Globals.PARTY_SLOT[1] < 9 && Globals.PARTY_SLOT[2] < 9) {
                 Globals.NO_DART = Globals.PARTY_SLOT[0];
                 emulator.WriteByteU(Constants.GetAddress("PARTY_SLOT") + Constants.OFFSET, 0);
             }
@@ -126,10 +126,24 @@ public class BattleController {
     public static int GetOffset() {
         int[] discOffset = { 0xD80, 0x0, 0x1458, 0x1B0 };
         int[] charOffset = { 0x0, 0x180, -0x180, 0x420, 0x540, 0x180, 0x350, 0x2F0, -0x180 };
+        int[] duoCharOffset = { 0x0, -0x180, 0x180, -0x420, -0x180, -0x540, -0x350, -0x350, -0x2F0 };
+        int[] duoDartOffset = { 0x0, 0x470, 0x170, 0x710, 0x830, 0x470, 0x640, 0x640, 0x170 };
         int partyOffset = 0;
-        if (Globals.PARTY_SLOT[0] < 9 && Globals.PARTY_SLOT[1] < 9 && Globals.PARTY_SLOT[2] < 9) {
-            partyOffset = charOffset[Globals.PARTY_SLOT[1]] + charOffset[Globals.PARTY_SLOT[2]];
+
+        if (Globals.PARTY_SLOT[2] == 255 && Globals.PARTY_SLOT[1] < 9) {
+            if (Globals.PARTY_SLOT[0] == 0) {
+                partyOffset = duoDartOffset[Globals.PARTY_SLOT[1]];
+            } else if (Globals.PARTY_SLOT[1] == 0) {
+                partyOffset = duoDartOffset[Globals.PARTY_SLOT[0]];
+            } else {
+                partyOffset = charOffset[Globals.PARTY_SLOT[0]] + charOffset[Globals.PARTY_SLOT[1]];
+            }
+        } else {
+            if (Globals.PARTY_SLOT[0] < 9 && Globals.PARTY_SLOT[1] < 9 && Globals.PARTY_SLOT[2] < 9) {
+                partyOffset = charOffset[Globals.PARTY_SLOT[1]] + charOffset[Globals.PARTY_SLOT[2]];
+            }
         }
+
         return discOffset[Globals.DISC - 1] - partyOffset;
     }
     
