e057783a2ba7805c207d44c2461c801f39ac2007
Zychronix <zychronix1@gmail.com>
2020-06-14 02:31:39 -0400

    .

---
+++ Dragoon Modifier/Scripts/Hotkeys/[ALL] Base.cs
@@ -0,0 +1,8 @@
+using Dragoon_Modifier;
+
+public class Base {
+    public static void Run(Emulator emulator) {}
+	public static void Open(Emulator emulator) {}
+	public static void Close(Emulator emulator) {}
+	public static void Click(Emulator emulator) {}
+}
