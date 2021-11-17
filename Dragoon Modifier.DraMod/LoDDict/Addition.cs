05ef3e2833765986745e594e0d042a9b5ae35e4a
Illeprih <Illeprih@gmail.com>
2021-11-17 14:20:32 +0100

    Addition stuff

---
+++ Dragoon Modifier.DraMod/LoDDict/Addition.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dragoon_Modifier.DraMod.LoDDict {
+    public class Addition {
+        public const ushort EndFlag = 0x4;
+        public const ushort RegularAddition = 0xC0;
+        public const ushort MasterAddition = 0xE0;
+
+        public readonly byte ID = 0;
+        public readonly byte Level = 0;
+        public readonly byte[] DamageIncrease = new byte[5];
+        public readonly byte[] SPIncrease = new byte[5];
+        public readonly byte StartTime = 0;
+
+        public readonly List<AdditionHit> AdditionHit = new List<AdditionHit>();
+
+        public Addition(string filePath) {
+            using(var file = new StreamReader(filePath)) {
+                var line = file.ReadLine().Split('\t').ToArray();
+
+                if (Byte.TryParse(line[1], out var bkey)) {
+                    ID = bkey;
+                }
+
+                line = file.ReadLine().Split('\t').ToArray();
+
+                if (Byte.TryParse(line[1], out bkey)) {
+                    Level = bkey;
+                }
+
+                file.ReadLine();
+                file.ReadLine();
+
+                line = file.ReadLine().Split('\t').ToArray();
+
+                for (int level = 0; level < 5; level++) {
+                    if (Byte.TryParse(line[level + 1], out bkey)) {
+                        DamageIncrease[level] = bkey;
+                    }
+                }
+
+                line = file.ReadLine().Split('\t').ToArray();
+
+                for (int level = 0; level < 5; level++) {
+                    if (Byte.TryParse(line[level + 1], out bkey)) {
+                        SPIncrease[level] = bkey;
+                    }
+                }
+
+                file.ReadLine();
+
+                line = file.ReadLine().Split('\t').ToArray();
+
+                if (Byte.TryParse(line[1], out bkey)) {
+                    StartTime = bkey;
+                }
+
+                file.ReadLine();
+
+                while (!file.EndOfStream) {
+                    line = file.ReadLine().Split('\t').ToArray();
+                    AdditionHit.Add(new AdditionHit(line));
+                }
+            }
+        }
+
+    }
+}
---
+++ Dragoon Modifier.DraMod/LoDDict/AdditionHit.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dragoon_Modifier.DraMod.LoDDict {
+    public class AdditionHit {
+        public readonly ushort Damage = 0;
+        public readonly ushort SP = 0;
+        public readonly byte BlueTime = 0;
+        public readonly byte GrayTime = 0;
+        public readonly byte CameraPanDistance = 0;
+        public readonly byte LockOnCameraDistance = 0;
+        public readonly byte LockOnCameraDistance2 = 0;
+        public readonly byte MonsterDistance = 0;
+        public readonly byte VerticaDistance = 0;
+        public readonly byte Unknown = 0;
+        public readonly byte Unknown2 = 0;
+
+        internal AdditionHit(string[] values) {
+            if (UInt16.TryParse(values[1], out var uskey)) {
+                Damage = uskey;
+            }
+
+            if (UInt16.TryParse(values[2], out uskey)) {
+                SP = uskey;
+            }
+
+            if (Byte.TryParse(values[3], out var bkey)) {
+                BlueTime = bkey;
+            }
+
+            if (Byte.TryParse(values[4], out bkey)) {
+                GrayTime = bkey;
+            }
+
+            if (Byte.TryParse(values[5], out bkey)) {
+                CameraPanDistance = bkey;
+            }
+
+            if (Byte.TryParse(values[6], out bkey)) {
+                LockOnCameraDistance = bkey;
+            }
+
+            if (Byte.TryParse(values[7], out bkey)) {
+                LockOnCameraDistance2 = bkey;
+            }
+
+            if (Byte.TryParse(values[8], out bkey)) {
+                MonsterDistance = bkey;
+            }
+
+            if (Byte.TryParse(values[9], out bkey)) {
+                VerticaDistance = bkey;
+            }
+
+            if (Byte.TryParse(values[10], out bkey)) {
+                Unknown = bkey;
+            }
+
+            if (Byte.TryParse(values[11], out bkey)) {
+                Unknown2 = bkey;
+            }
+
+        }
+    }
+}
